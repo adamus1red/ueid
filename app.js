2e0600d6474b8abdeea81a191dd50a775e90ff9b
Adam McGhie <Adam McGhie>
2016-06-14 17:26:11 +0100

    change user path to /u

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/u', users);
 app.use('/e', entity);
 require('./routes/login')(app, passport); // load our routes and pass in our app and fully configured passport
 // catch 404 and forward to error handler
