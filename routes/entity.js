4b6e45ec1b9b2144c084658d89cd56b948be201e
Adam McGhie <Adam McGhie>
2016-06-14 12:15:56 +0100

    make req.params lower case & make entity creation use one of the random prefixes

--- routes/entity.js
+++ routes/entity.js
@@ -6,28 +6,33 @@ var MongoClient = require('mongodb').MongoClient;
 var Common = require('../model/common'), Entity = require('../model/entity'), Prefix = require('../model/prefix');
 
 router.get('/test_create_entity', function(req,res,next) {
-    var rand = Math.floor((Math.random() * 1048575) + 1)
-    var test = new Entity({
-        "entity": {
-            "decimal": rand,
-            "hex": hex2dec(rand),
-            "fullUEID": "00:000001:" + hex2dec(rand)
-        }
-    });
-    test.save(function(err) {
-      if (err) throw err;
+    var rand = Math.floor((Math.random() * hex2dec("FFFFF")) + 1);
+    Prefix.find({}, function(err, data) {
+        var id = Math.floor((Math.random() * data.length))
+        var test = new Entity({
+            "entity": {
+                "decimal": rand,
+                "hex": dec2hex(rand),
+                "fullUEID": "00:"+data[id].hex.toLowerCase()+":" + dec2hex(rand)
+            },
+            "prefix": data[id]._id
+        });
+        test.save(function(err) {
+          if (err) throw err;
 
-      console.log('Entity saved successfully!');
+          console.log('Entity saved successfully!');
+        });
     });
+    
     res.send("Test object created");
     res.end();
 });
 
 router.get('/test_create_prefix', function(req,res,next) {
-    var rand = Math.floor((Math.random() * 16777215) + 1)
+    var rand = Math.floor((Math.random() * hex2dec("FFFFFF")) + 1);
     var test = new Prefix({
         "decimal": rand,
-        "owner" :  "575f54602f7b00683af231c9",
+        "owner" :  "575de2ce690540882cfc0018",
         "hex": dec2hex(rand),
         "name": "Test Prefix",
         "description": "Prefix to be used for the development and testing of UEID systems",
@@ -83,7 +88,7 @@ router.get('/', function(req, res, next) {
 router.get('/:type', function(req,res,next) {
     Common.findOne({"name": "types"}, function(err, data) {
         if (err) throw err;
-        Prefix.find({"type.hex": leftpad(req.params.type, 2, 0)}).populate('owner').exec(function(err, prefix) {
+        Prefix.find({"type.hex": leftpad(req.params.type.toLowerCase(), 2, 0)}).populate('owner').exec(function(err, prefix) {
             console.log(data);
             if(prefix.length>0 && prefix != null){
                 res.render('type', { title: 'Type Entity', type: req.params.type, data: data, prefixi: prefix, user: req.user});
@@ -106,15 +111,17 @@ router.get('/:type', function(req,res,next) {
 });
 
 router.get('/:type/:prefix', function(req, res, next) {
-    console.log(leftpad(req.params.prefix, 6, 0));
-    Prefix.findOne({'hex': leftpad(req.params.prefix, 6, 0), 'type.hex': leftpad(req.params.type, 2, 0)}).populate('owner').exec(function(err,prefix) {
+    console.log(leftpad(req.params.prefix.toLowerCase(), 6, 0));
+    Prefix.findOne({'hex': leftpad(req.params.prefix.toLowerCase(), 6, 0), 'type.hex': leftpad(req.params.type.toLowerCase(), 2, 0)}).populate('owner').exec(function(err,prefix) {
         if (err) throw err;
-        console.log("prefix ID "+ prefix._id)
+        console.log("prefix: " + JSON.stringify(prefix));
         Entity.find({"prefix":prefix._id}).populate('prefix').exec(function (err, entity) {
             if (err) throw err;
             // object of all the stuff
-            console.log(JSON.stringify(entity));
-            if(typeof entity != "undefined" && entity != null){
+            console.log("Entity " + JSON.stringify(entity));
+            if (entity == []) {
+                res.render('prefix', { title: 'Prefix Entity', type: req.params.type, prefix: prefix, entities: null, user: req.user });
+            } else if(typeof entity != "undefined" && entity != null && entity != []){
                 res.render('prefix', { title: 'Prefix Entity', type: req.params.type, prefix: prefix, entities: entity, user: req.user });
             } else {
                 res.status(404).send('Not found');
@@ -124,7 +131,7 @@ router.get('/:type/:prefix', function(req, res, next) {
 });
 
 router.get('/:type/:prefix/:eid', function(req, res, next) {
-    Entity.findOne({ 'entity.fullUEID': leftpad(req.params.type, 2, 0) + ":" + leftpad(req.params.prefix, 6, 0) + ":" + leftpad(req.params.eid,5,0)}).populate('prefix').exec( function(err,entity) {
+    Entity.findOne({ 'entity.fullUEID': leftpad(req.params.type.toLowerCase(), 2, 0) + ":" + leftpad(req.params.prefix.toLowerCase(), 6, 0) + ":" + leftpad(req.params.eid.toLowerCase(),5,0)}).populate('prefix').exec( function(err,entity) {
         if (err) throw err;
 
         // object of all the stuff
@@ -154,12 +161,12 @@ function getPrefixInfo(prefixHex) {
     return q;
 }
 
-function hex2dec(number){
-    return number.toString(16);
+function dec2hex(number){
+    return number.toString(16).toLowerCase();
 }
 
-function dec2hex(hex){
-    return parseInt(hex, 16);
+function hex2dec(hex){
+    return parseInt(hex.toLowerCase(), 16);
 }
 
 module.exports = router;
