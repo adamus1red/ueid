ec998d267a4fa1fd304ff0e1c1379518bce6cb41
Adam McGhie <Adam McGhie>
2016-07-03 15:19:19 +0100

    add proper landing page for new logins

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,17 @@ var validator = require('validator');
 var Common = require('../model/common'), Entity = require('../model/entity'), Prefix = require('../model/prefix'), User = require('../model/user');
 /* GET users listing. */
 router.get('/', loggedIn, function(req, res, next) {
-    res.send(JSON.stringify(req.user, null, '    '));
+    User.findOne({'username': req.user.username}, function(err,us) {
+        if(err) {
+            res.status(500).send("Error looking up user");
+        }
+        Prefix.find({'owner': us._id}, function(err,prefixes){
+            if(err) {
+                res.status(500).send("Error looking up assigned prefixes");
+            }
+            res.render('userProfile', {title: "User Profile" + req.params.user, user: req.user, assignedPrefixes: prefixes, username: us});
+        });
+    });
 });
 // User dashboard
 router.get('/dashboard', loggedIn, function(req, res, next) {
